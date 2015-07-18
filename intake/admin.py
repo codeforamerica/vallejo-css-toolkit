08d35962dbc0ba964a5bc815c61e1896fdeff98e
Andrew Schneider <adschneider@gmail.com>
2015-07-18 10:58:40 -0700

    fixes

--- intake/admin.py
+++ intake/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from intake.models import Call
 
 class CallAdmin(admin.ModelAdmin):
-    pass
+    fields = ('call_sid', 'caller_name', 'name_recording_url', 'caller_number', 'caller_preferred_contact', 'problem_address', 'address_recording_url', 'problem_description', 'description_recording_url',)
+
 
 admin.site.register(Call, CallAdmin)
--- intake/models.py
+++ intake/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Call(models.Model):
-    call_sid = models.CharField(max_length=256, primary_key=True)
+    call_sid = models.CharField(max_length=256, null=True)
     caller_name = models.CharField(max_length=256, null=True)
     name_recording_url = models.CharField(max_length=256, null=True)
-    caller_number = models.BigIntegerField()
+    caller_number = models.BigIntegerField(null=True)
     call_time = models.DateTimeField(auto_now_add=True)
     caller_preferred_contact = models.IntegerField(null=True)
     problem_address = models.CharField(max_length=256, null=True)
