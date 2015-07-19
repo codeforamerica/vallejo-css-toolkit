0959d8d471b469933371b2dba5cf29f64749fd04
Andrew Schneider <adschneider@gmail.com>
2015-07-19 12:32:02 -0700

    tweaking admin layout

--- intake/admin.py
+++ intake/admin.py
@@ -4,6 +4,7 @@ from intake.models import Call
 
 class CallAdmin(admin.ModelAdmin):
     fields = ('call_sid', 'caller_name', 'name_recording_url', 'caller_number', 'caller_preferred_contact', 'problem_address', 'address_recording_url', 'problem_description', 'description_recording_url',)
-
+    readonly_fields = ('call_time')
+    list_display = ('call_sid', 'call_time', 'caller_name', 'caller_number', 'problem_address',)
 
 admin.site.register(Call, CallAdmin)
