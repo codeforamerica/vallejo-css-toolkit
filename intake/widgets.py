931854868f9fd335e088d1350d4752bd3b521aa0
Andrew Schneider <adschneider@gmail.com>
2015-07-21 11:54:34 -0700

    admin tweak

--- intake/widgets.py
+++ intake/widgets.py
@@ -6,19 +6,19 @@ class NameRecordingFieldWidget(forms.TextInput):
      def render(self, name, value, attrs=None):
         html = super(NameRecordingFieldWidget, self).render(name, value, attrs)
         if value:
-            html =  """<audio controls><source src="%s.mp3" type="audio/mpeg"></audio><br/>""" % value
+            html =  """<audio controls><source src="%s.mp3" type="audio/mpeg"></audio><br/>""" % value + html.replace(' ', ' hidden ', 1)
         return mark_safe(html)
 
 class AddressRecordingFieldWidget(forms.TextInput):
      def render(self, name, value, attrs=None):
         html = super(AddressRecordingFieldWidget, self).render(name, value, attrs)
         if value:
-            html =  """<audio controls><source src="%s.mp3" type="audio/mpeg"></audio><br/>""" % value
+            html =  """<audio controls><source src="%s.mp3" type="audio/mpeg"></audio><br/>""" % value + html.replace(' ', ' hidden ', 1)
         return mark_safe(html)
 
 class DescriptionRecordingFieldWidget(forms.TextInput):
      def render(self, name, value, attrs=None):
         html = super(DescriptionRecordingFieldWidget, self).render(name, value, attrs)
         if value:
-            html =  """<audio controls><source src="%s.mp3" type="audio/mpeg"></audio><br/>""" % value
+            html =  """<audio controls><source src="%s.mp3" type="audio/mpeg"></audio><br/>""" % value + html.replace(' ', ' hidden ', 1)
         return mark_safe(html)
