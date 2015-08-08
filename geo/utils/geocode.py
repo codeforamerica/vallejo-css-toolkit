4324fa693be9f226dbd97237160e4131c45808d1
Andrew Schneider <adschneider@gmail.com>
2015-08-08 16:24:35 -0700

    moving scripts over as django management commands

--- geo/utils/geocode.py
+++ geo/utils/geocode.py
@@ -24,12 +24,15 @@ def geocode_try_nearby(street_number, street_name, street_descriptor=None):
     modulo = street_number % 2
 
     query = """
-        select lat, lng, street_number from geo_address
-        where lower(street_name) = lower(%s)
-        and abs(street_number - %s) < 10
-        order by abs(street_number - %s)
+        select lat, lng, street_number
+            from geo_address
+        where
+            lower(street_name) = lower(%s)
+            and abs(street_number - %s) < 10
+        order by
+            abs(street_number - %s)
         limit 1;
-    """
+        """
     params = [street_name, street_number, street_number]
 
     # only supporting this case for now...
@@ -50,11 +53,17 @@ def geocode_try_interpolate(street_number, street_name, street_descriptor=None):
 
     modulo = street_number % 2
 
-    query = """select street_number, lat, lng from geo_address
-    where lower(street_name) = lower(%s) and street_number %s %s and street_number %% 2 = %s
-    order by street_number %s
-    limit 2
-    """
+    query = """
+        select street_number, lat, lng
+            from geo_address
+        where
+            lower(street_name) = lower(%s)
+            and street_number %s %s
+            and street_number %% 2 = %s
+        order by
+            street_number %s
+        limit 2
+        """
     params = [street_name]
 
     # only supporting this case for now...
@@ -63,7 +72,6 @@ def geocode_try_interpolate(street_number, street_name, street_descriptor=None):
 
     cursor = connection.cursor()
     cursor.execute(query, params + [AsIs('>'), street_number, modulo, AsIs('asc')])
-    # print cursor.mogrify(query, params + [AsIs('>'), AsIs('street_number'), AsIs('asc')])
     results = dictfetchall(cursor)
 
     if results:
@@ -73,15 +81,12 @@ def geocode_try_interpolate(street_number, street_name, street_descriptor=None):
 
     cursor = connection.cursor()
     cursor.execute(query, params + [AsIs('<'), street_number, modulo, AsIs('desc')])
-    # print cursor.mogrify(query, params + [AsIs('<'), AsIs('street_number'), AsIs('desc')])
     results = dictfetchall(cursor)
 
     if results:
         first_lower = results[0]
     else:
         first_lower = None
-
-    # print first_higher, first_lower
 
     if first_higher and first_lower:
         intersect = float(street_number - first_lower['street_number']) / (first_higher['street_number'] - first_lower['street_number'])
@@ -96,20 +101,19 @@ def geocode_try_interpolate(street_number, street_name, street_descriptor=None):
 
 def geocode_try_exact(street_number, street_name, street_descriptor=None):
 
-
-    base_query = """select lat, lng from geo_address
-    where lower(street_name) = lower(%s) and street_number = %s
-    """
-    base_params = [street_name, street_number]
+    query = """
+        select lat, lng
+            from geo_address
+        where
+            lower(street_name) = lower(%s)
+            and street_number = %s
+        """
+    params = [street_name, street_number]
 
     if street_descriptor:
 
-        query = base_query + """ AND lower(street_descriptor) = %s """
-        params = base_params + [street_descriptor]
-
-    else:
-        query = base_query
-        params = base_params
+        query = "%s %s" % (query, """AND lower(street_descriptor) = %s""")
+        params += [street_descriptor]
 
     cursor = connection.cursor()
     cursor.execute(query, params)
@@ -174,58 +178,3 @@ def geocode(street_number, street_name, street_descriptor=None):
 
         else:
             pass
-            # print street_number, street_name, len(street_name)
-
-
-def test_geocode(filepath=None):
-
-    results = {
-        'bad street num': 0,
-        'bad row': 0,
-        'exact hit': 0,
-        'total': 0
-    }
-
-    with open(filepath, 'rb') as f:
-        dialect = csv.Sniffer().sniff(f.read(1048576), delimiters=",")
-        f.seek(0)
-        reader = csv.reader(f, dialect)
-
-        next(reader)
-
-        for i, row in enumerate(reader):
-            street_number = row[1]
-            street_name = row[2]
-            results['total'] += 1
-
-            if not street_number and street_name:
-                # print 'bad row: ', row
-                results['bad row'] += 1
-                continue
-
-            if not street_number.isdigit():
-                # print 'bad street_number: ', street_number
-                results['bad street num'] += 1
-                continue
-
-            street_number = int(street_number)
-            x = geocode(street_number, street_name)  
-            if x:
-                results['exact hit'] += 1
-
-            else:
-                print street_number, street_name, len(street_name)
-
-    print results
-
-if __name__ == "__main__":
-
-    func_name = sys.argv[1]
-
-    if func_name == 'import_openaddresses':
-        filepath = sys.argv[2]
-        import_openaddresses(filepath)
-
-    if func_name == 'test_geocode':
-        filepath = sys.argv[2]
-        test_geocode(filepath)
