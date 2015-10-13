5b6e8f66f0540cd6a59108fea5832be7ea7fa7f2
Andrew Schneider <adschneider@codeforamerica.org>
2015-10-13 16:33:12 -0700

    forgot setting it in staging env

--- vallejo_css_toolkit/staging.py
+++ vallejo_css_toolkit/staging.py
@@ -4,3 +4,9 @@ from .settings import *
 
 DATABASES = {'default': dj_database_url.config()}
 DEBUG = True
+
+STATIC_LIB_BASE = "https://s3.amazonaws.com/vallejo-css-toolkit/static/libs/"
+
+SETTINGS_EXPORT = [
+    'STATIC_LIB_BASE',
+]
