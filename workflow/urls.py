c54dd87c568d6040f63d5c646ddc9f8960d895e8
Andrew Schneider <adschneider@codeforamerica.org>
2015-11-09 10:37:26 -0800

    view fix

--- workflow/urls.py
+++ workflow/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns(
     # case related views
     url(r'^workflow/case/(?P<case_id>\d*)/$', 'workflow.views.case_views.case', name='case'),
     url(r'^workflow/cases/$', 'workflow.views.case_views.cases', name='cases'),
-    url(r'^workflow/cases_data/$', 'workflow.views.case_views.cases_data', name='cases_data'),
     url(r'^workflow/add_case_assignee/$', 'workflow.views.case_views.add_case_assignee', name='add_case_assignee'),
     url(r'^workflow/remove_case_assignee/$', 'workflow.views.case_views.remove_case_assignee', name='remove_case_assignee'),
 
@@ -16,7 +15,6 @@ urlpatterns = patterns(
     # report related views
     url(r'^workflow/report/(?P<report_id>\d*)/$', 'workflow.views.report_views.report', name='report'),
     url(r'^workflow/reports/$', 'workflow.views.report_views.reports', name='reports'),
-    url(r'^workflow/reports_data/$', 'workflow.views.report_views.reports_data', name='reports_data'),
     url(r'^workflow/add_report/$', 'workflow.views.report_views.add_report', name='add_report'),
     url(r'^workflow/resolve_report/(?P<report_id>\d*)/$', 'workflow.views.report_views.resolve_report', name='resolve_report'),
     url(r'^workflow/verify_report/(?P<verification_id>\d*)/$', 'workflow.views.report_views.verify_report', name='verify_report'),
