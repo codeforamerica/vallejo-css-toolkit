38a21769c2824e19b1f7ba542d7bfe2232f19136
Andrew Schneider <adschneider@codeforamerica.org>
2015-11-09 12:05:55 -0800

    etl endpoint fix

--- data_load/urls.py
+++ data_load/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     # endpoint for ETL process to check crw high water mark
-    url(r'^get_latest_crw_case_nos/$', 'data_load.views.get_latest_crw_case_nos', name='get_latest_crw_case_no'),
+    url(r'^get_latest_crw_case_no/$', 'data_load.views.get_latest_crw_case_no', name='get_latest_crw_case_no'),
 
     # endpoint for ETL process to check rms_high water mark
     url(r'^get_latest_rms_case_no/$', 'data_load.views.get_latest_rms_case_no', name='get_latest_rms_case_no'),
