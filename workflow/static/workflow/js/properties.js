779b6a0344c50e0c42339c89298e52b2aebb61a5
Andrew Schneider <adschneider@codeforamerica.org>
2015-11-09 07:57:16 -0800

    disable single property view for now

--- workflow/static/workflow/js/properties.js
+++ workflow/static/workflow/js/properties.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $('#properties-nav-tab').addClass('active');
 
-    $(".clickable-cell").click(function(e) {
-        if ($(this).data("href") !== undefined) {
-            window.document.location = $(this).data("href");
-        }
-    });
+    // $(".clickable-cell").click(function(e) {
+    //     if ($(this).data("href") !== undefined) {
+    //         window.document.location = $(this).data("href");
+    //     }
+    // });
 
 });
