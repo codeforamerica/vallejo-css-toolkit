773777c707af83a5dab685471d0d2953a1616e3a
Andrew Schneider <adschneider@codeforamerica.org>
2015-11-13 14:12:16 -0800

    debugging

--- vallejo_css_toolkit/production.py
+++ vallejo_css_toolkit/production.py
@@ -3,7 +3,7 @@ import dj_database_url
 from .settings import *
 
 DATABASES = {'default': dj_database_url.config()}
-DEBUG = True
+DEBUG = False
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
