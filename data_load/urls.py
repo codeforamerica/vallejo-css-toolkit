3c47c68ad94575be86440e67ef8d422533275b1c
Andrew Schneider <adschneider@codeforamerica.org>
2015-11-19 13:41:36 -0800

    more etl fixes

--- data_load/urls.py
+++ data_load/urls.py
@@ -5,8 +5,11 @@ urlpatterns = patterns(
     # endpoint for ETL process to check crw high water mark
     url(r'^get_latest_crw_case_no/$', 'data_load.views.get_latest_crw_case_no', name='get_latest_crw_case_no'),
 
-    # endpoint for ETL process to check rms_high water mark
+    # endpoint for ETL process to check rms high water mark
     url(r'^get_latest_rms_case_no/$', 'data_load.views.get_latest_rms_case_no', name='get_latest_rms_case_no'),
+
+    # endpoint for ETL process to check rms high water mark
+    url(r'^get_latest_rms_incident_no/$', 'data_load.views.get_latest_incident_case_no', name='get_latest_rms_incident_no'),
 
     # endpoint for ETL process to post crw updates
     url(r'^handle_crw_post/$', 'data_load.views.handle_crw_post', name='handle_crw_post'),
